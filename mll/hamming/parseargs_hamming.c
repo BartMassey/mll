bb9da33e9a6d3092f7599f2f08f0aef6e0002e38
thomure <thomure>
2003-10-23 23:07:53 +0000

    no parameters specific to hamming so no params struct defined or created by parseargs_hamming().

---
+++ mll/hamming/parseargs_hamming.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <string.h>
+
+#include "../mll.h"
+
+static char *usage_msg = "learner: hamming: usage: -a hamming";
+
+static void usage(void) {
+    fprintf(stderr, "%s\n", usage_msg);
+    exit(1);
+}
+
+struct params *parseargs_hamming(int argc, char **argv) {
+    int ch;
+
+    return NULL;
+}
+
