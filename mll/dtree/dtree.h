10dbd998ae13e2834759facc4d6eb6f5df5a47d6
thomure <thomure>
2003-10-24 00:31:45 +0000

    *** empty log message ***

--- mll/dtree/dtree.h
+++ mll/dtree/dtree.h
@@ -2,13 +2,7 @@
  * Private definitions for dtree learner
  */
 
-/* index is sign of instance */
-typedef int count[2];
-
 struct knowledge {
-    int nsign[2];
-    int nconditions;
-    count *counts;
 };
 
 struct params {
