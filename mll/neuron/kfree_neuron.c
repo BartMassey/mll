c4c895a7ec9c22444e46e07ea4e9586279b2f123
thomure <thomure>
2003-10-24 01:16:21 +0000

    freed all dynamic arrays

--- mll/neuron/kfree_neuron.c
+++ mll/neuron/kfree_neuron.c
@@ -6,4 +6,11 @@
 void kfree_neuron(struct knowledge *k) {
     free(k->weights);
     free(k);
+
+    if (k->delta)
+	free(k->delta);
+    if (k->best_weights)
+	free(k->best_weights);
+    if (k->averages)
+	free(k->averages);
 }
