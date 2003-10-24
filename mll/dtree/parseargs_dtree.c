3c5f804457206c921f9034962f010a43331857b6
thomure <thomure>
2003-10-24 00:31:22 +0000

    wasn't converting ascii to floats on parameters

--- mll/dtree/parseargs_dtree.c
+++ mll/dtree/parseargs_dtree.c
@@ -10,6 +10,8 @@ static char *usage_msg = "learner: dtree: usage: -a dtree";
 
 static char *options = "";
 static struct option long_options[] = {
+    {"mingain", 1, 0, 'g'},
+    {"minchisquare", 1, 0, 'c'},
     {0, 0, 0, 0}
 };
 
@@ -20,10 +22,9 @@ static void usage(void) {
 
 struct params *parseargs_dtree(int argc, char **argv) {
     int ch;
-    struct params *p = malloc((size_t) sizeof(struct params));
+    struct params *p = malloc(sizeof (*p));
 
-    if (!p)
-	return NULL;
+    assert(p);
 
     // Init with default values
     p->min_gain = 0.001;
@@ -31,14 +32,8 @@ struct params *parseargs_dtree(int argc, char **argv) {
 
     while ((ch = getopt_long(argc, argv, options, long_options, 0)) > 0) {
         switch(ch) {
-	case 'g':
-	    p->min_gain = optarg;
-	    break;
-
-	case 'c':
-	    p->min_chisquare = optarg;
-	    break;
-
+	case 'g':  p->min_gain = atof(optarg); break;
+	case 'c':  p->min_chisquare = atof(optarg); break;
         default:  usage();
         }
     }
