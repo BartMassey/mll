3338e20b70c018bf0ad85acf0718c5bb29aed6a7
thomure <thomure>
2003-10-24 00:35:14 +0000

    added makefile for netclass learner

--- mll/netclass/netclass.h
+++ mll/netclass/netclass.h
@@ -2,12 +2,6 @@
  * Private definitions for neclass learner
  */
 
-/* index is sign of instance */
-typedef int count[2];
-
 struct knowledge {
-    int nsign[2];
-    int nconditions;
-    count *counts;
 };
 
