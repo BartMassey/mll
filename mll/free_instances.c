1ecd3489bd7945fb7905deb51a2bfb830daa013f
thomure <thomure>
2003-10-27 00:04:58 +0000

    forgot to free instance struct itself

--- mll/free_instances.c
+++ mll/free_instances.c
@@ -16,6 +16,7 @@ void free_instances(struct instances *iip) {
     for (i=0; i < iip->ninstances; i++) {
 	bs_free(iip->instances[i]->conditions);
 	free(iip->instances[i]->name);
+	free(iip->instances[i]);
     }
 
     free(iip->instances);
