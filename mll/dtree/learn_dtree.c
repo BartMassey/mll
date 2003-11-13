b1a5e4e972dd858d88d11b82bc59dd0861ad5b35
thomure <thomure>
2003-11-13 23:03:22 +0000

    still need to port train() code from dtree.c to learn_dtree.c

--- mll/dtree/learn_dtree.c
+++ mll/dtree/learn_dtree.c
@@ -5,37 +5,37 @@
 #include "dtree.h"
 
 
-struct knowledge *learn_dtree(struct instances *ip,
+struct knowledge *learn_dtree(struct instances *iip,
 			      struct params *p) {
     int i, j;
     bitset no_attrs;
     struct knowledge *k = malloc(sizeof(*k));
 
     assert(k);
-    k->ninstances = ip->ninstances;
+    k->ninstances = iip->ninstances;
 
 // init_masks
-    k->instance_mask = bs_mask(ip->ninstances);
-    k->condition_mask = bs_mask(ip->nconditions);
+    k->instance_mask = bs_mask(iip->ninstances);
+    k->condition_mask = bs_mask(iip->nconditions);
 
 // init_instances
-    k->pos_instances = bs_new(ip->ninstances);
-    for (i = 0; i < ip->ninstances; i++)
-        if (ip->instances[i].sign > 0)
+    k->pos_instances = bs_new(iip->ninstances);
+    for (i = 0; i < iip->ninstances; i++)
+        if (iip->instances[i]->sign > 0)
             bs_lset(k->pos_instances, i);
     k->neg_instances = bs_not(k->pos_instances);
     bs_land(k->neg_instances, k->instance_mask);
 
 // init_condition_cache
-    k->pos_conditions = malloc(ip->nconditions * sizeof(bitset));
+    k->pos_conditions = malloc(iip->nconditions * sizeof(bitset));
     assert(k->pos_conditions);
-    k->neg_conditions = malloc(ip->nconditions * sizeof(bitset));
+    k->neg_conditions = malloc(iip->nconditions * sizeof(bitset));
     assert(k->neg_conditions);
-    for (i = 0; i < ip->nconditions; i++) {
+    for (i = 0; i < iip->nconditions; i++) {
         bitset ntmp, ptmp;
-        ptmp = bs_new(ip->ninstances);
-        for (j = 0; j < ip->ninstances; j++) {
-            if (bs_isset(ip->instances[j].conditions, i))
+        ptmp = bs_new(iip->ninstances);
+        for (j = 0; j < iip->ninstances; j++) {
+            if (bs_isset(iip->instances[j]->conditions, i))
                 bs_lset(ptmp, j);
         }
         k->pos_conditions[i] = ptmp;
@@ -45,8 +45,8 @@ struct knowledge *learn_dtree(struct instances *ip,
     }
 
 // xtras
-    no_attrs = bs_new(nconditions);
-    k->tree = make_dtree(k->no_attrs, k->instance_mask);
+    no_attrs = bs_new(iip->nconditions);
+    k->tree = make_dtree(no_attrs, k->instance_mask);
 
     // XXX not using [no_attrs] anymore?
     bs_free(no_attrs);
