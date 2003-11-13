06d0251212c9d264b515d32f22347e6ef4665097
thomure <thomure>
2003-11-13 23:31:56 +0000

    added skeleton version of {read,write}_dtree.c

---
+++ mll/dtree/read_dtree.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+#include "../mll.h"
+#include "dtree.h"
+
+/* XXX should be changed to use XML */
+
+struct knowledge *read_dtree(FILE *f) {
+}
---
+++ mll/dtree/write_dtree.c
@@ -0,0 +1,18 @@
+#include "../mll.h"
+#include "dtree.h"
+
+/* XXX should be changed to use XML */
+/* XXX should be able to fail */
+
+int write_dtree(FILE *f, struct knowledge *k) {
+    // write ninstances
+
+    // write instance_mask
+    // write condition_mask
+
+    // write pos_instances
+    // write neg_instances
+
+    // write pos_conditions
+    // write neg_conditions
+}
