576fcf498398ba0979f8ac89a52b39349c771276
thomure <thomure>
2003-11-21 01:39:08 +0000

    Commented out usage() function since it was producing warnings about "never being used"...

--- mll/hamming/parseargs_hamming.c
+++ mll/hamming/parseargs_hamming.c
@@ -6,12 +6,14 @@
 
 #include "../mll.h"
 
+/*
 static char *usage_msg = "learner: hamming: usage: -a hamming";
 
 static void usage(void) {
     fprintf(stderr, "%s\n", usage_msg);
     exit(1);
 }
+*/
 
 struct params *parseargs_hamming(int argc, char **argv) {
     return NULL;
