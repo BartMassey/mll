7ecb06af741bec1469de6bdd0778081c50f73cb0
thomure <thomure>
2003-11-21 01:41:29 +0000

    Added comments and broke out init_* code to seperate functions like they were in dtree.c

--- mll/dtree/learn_dtree.c
+++ mll/dtree/learn_dtree.c
@@ -5,47 +5,60 @@
 #include "dtree.h"
 
 
-struct knowledge *learn_dtree(struct instances *iip,
-			      struct params *p) {
+/********************************************************
+ * PARAM:
+ * DESC: 
+ * RETURN: 
+ ********************************************************/
+void init_masks(struct knowledge *k) {
+    k->instance_mask = bs_mask(k->ninstances);
+    k->condition_mask = bs_mask(k->nconditions);
+}
 
-    init_masks();
-    init_instances();
-    init_condition_cache();
+/********************************************************
+ * PARAM:
+ * DESC: 
+ * RETURN: 
+ ********************************************************/
+void init_instances(struct knowledge *k, 
+                    struct instances *iip) {
+    int i;
 
-    no_attrs = bs_new(nconditions);
-    // Start with initial tree with no
-    d = make_dtree(no_attrs, instance_mask);
-
-
-
-    int i, j;
-    bitset no_attrs;
-    struct knowledge *k = malloc(sizeof(*k));
-
-    assert(k);
-    k->ninstances = iip->ninstances;
-
-// init_masks
-    k->instance_mask = bs_mask(iip->ninstances);
-    k->condition_mask = bs_mask(iip->nconditions);
-
-// init_instances
-    k->pos_instances = bs_new(iip->ninstances);
-    for (i = 0; i < iip->ninstances; i++)
+    // Create mask of positive instances
+    k->pos_instances = bs_new(k->ninstances);
+    for (i = 0; i < k->ninstances; i++)
         if (iip->instances[i]->sign > 0)
             bs_lset(k->pos_instances, i);
-    k->neg_instances = bs_not(k->pos_instances);
-    bs_land(k->neg_instances, k->instance_mask);
 
-// init_condition_cache
+    // Create negation of positive instances
+    k->neg_instances = bs_not(k->pos_instances);
+
+    // AND negative instances with instance_mask
+    // XXX Bart, is this necessary since all bits 
+    //     in [instance_mask] are set?
+    bs_land(k->neg_instances, k->instance_mask);
+}
+
+/********************************************************
+ * PARAM:
+ * DESC: 
+ * RETURN: 
+ ********************************************************/
+void init_condition_cache(struct knowledge *k, 
+                          struct instances *iip) {
+    int i, j;
+
+    // Create bitset arrays of size [nconditions]
     k->pos_conditions = malloc(iip->nconditions * sizeof(bitset));
     assert(k->pos_conditions);
     k->neg_conditions = malloc(iip->nconditions * sizeof(bitset));
     assert(k->neg_conditions);
+
     for (i = 0; i < iip->nconditions; i++) {
+        // Set which instances displayed condition [i]
         bitset ntmp, ptmp;
-        ptmp = bs_new(iip->ninstances);
-        for (j = 0; j < iip->ninstances; j++) {
+        ptmp = bs_new(k->ninstances);
+        for (j = 0; j < k->ninstances; j++) {
             if (bs_isset(iip->instances[j]->conditions, i))
                 bs_lset(ptmp, j);
         }
@@ -54,12 +67,30 @@ struct knowledge *learn_dtree(struct instances *iip,
         bs_land(ntmp, k->instance_mask);
         k->neg_conditions[i] = ntmp;
     }
+}
 
+/********************************************************
+ * PARAM:
+ * DESC: 
+ * RETURN: 
+ ********************************************************/
+struct knowledge *learn_dtree(struct instances *iip,
+			      struct params *p) {
+    bitset no_attrs;
+    struct knowledge *k = malloc(sizeof(*k));
+
+    assert(k);
+    k->ninstances = iip->ninstances;
+    k->nconditions = iip->nconditions;
+
+    init_masks(k);
+    init_instances(k, iip);
+    init_condition_cache(k, iip);
 
     no_attrs = bs_new(iip->nconditions);
 
     // Make empty dtree
-    k->tree = make_dtree(no_attrs, k->instance_mask);
+    k->tree = make_dtree(k, p, no_attrs, k->instance_mask);
 
     bs_free(no_attrs);
 
