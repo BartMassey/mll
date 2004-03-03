134101750f1a0b7f1938405eb5cb1d0c24db1b5e
thomure <thomure>
2004-03-03 22:41:33 +0000

    Added optional printing of classification confidence to classify_dtree()

--- mll/dtree/classify_dtree.c
+++ mll/dtree/classify_dtree.c
@@ -6,22 +6,19 @@
 #include "dtree.h"
 
 
-dtree_type classify_subtree(dtree *d, struct instance *ip) {
+dtree *classify_subtree(dtree *d, struct instance *ip) {
     switch(d->tag) {
-    case DT_POS:
-    case DT_NEG:
-        return d->tag;
-    case DT_MIXED:
-        if (d->val.leaf.pos > d->val.leaf.neg)
-            return DT_POS;
-        if (d->val.leaf.pos < d->val.leaf.neg)
-            return DT_NEG;
-        return DT_MIXED;
     case DT_NODE:
         if (bs_isset(ip->conditions, d->val.node.attr))
             return classify_subtree(d->val.node.pos, ip);
         return classify_subtree(d->val.node.neg, ip);
-    case DT_EMPTY:
+
+    case DT_POS:
+    case DT_NEG:
+    case DT_MIXED:
+        return d;
+
+    default:
         abort();
     }
     abort();
@@ -31,13 +28,39 @@ dtree_type classify_subtree(dtree *d, struct instance *ip) {
 int classify_dtree(struct knowledge *k,
 		   struct instance *ip,
 		   struct params *p) {
+    int class;
+    float conf;
+    dtree *d = classify_subtree(k->tree, ip);
 
-    dtree_type c = classify_subtree(k->tree, ip);
+    // non-mixed leaf node, return given classification
+    if (d->tag == DT_POS || d->tag == DT_NEG) {
+        conf = 1;
 
-    if (c == DT_POS)
-      return 1;
-    if (c == DT_NEG)
-      return -1;
+        if (d->val.leaf.pos > d->val.leaf.neg)
+            class = 1;
+        if (d->val.leaf.pos < d->val.leaf.neg)
+            class = -1;
 
-    return 0;
+    // mixed node, return most common classification
+    } else if (d->tag == DT_MIXED) {
+        // Get absolute value of conf
+        conf = fabs( ((float)d->val.leaf.pos - d->val.leaf.neg) / (d->val.leaf.pos + d->val.leaf.neg) );
+
+        if (d->val.leaf.pos > d->val.leaf.neg)
+            class = 1;
+        else if (d->val.leaf.pos < d->val.leaf.neg)
+            class = -1;
+        else
+            class = 0;
+
+    // bad node type
+    } else {
+        conf = 0.5;
+        class = 0;
+    }
+
+    if (p->conf)
+        printf("conf: %f\n", conf);
+
+    return class;
 }
